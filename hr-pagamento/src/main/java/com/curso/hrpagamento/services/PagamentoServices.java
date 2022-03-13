90723a5dd052f16ebbe43a0dc7d270fc658bd2d8
dulcidio <dulcidio.sobrinho@gmail.com>
2022-03-13 01:16:12 -0300

    RestTemplate Basico

--- hr-pagamento/src/main/java/com/curso/hrpagamento/services/PagamentoServices.java
+++ hr-pagamento/src/main/java/com/curso/hrpagamento/services/PagamentoServices.java
@@ -1,14 +1,32 @@
 package com.curso.hrpagamento.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.curso.hrpagamento.entities.Pagamento;
+import com.curso.hrpagamento.entities.Worker;
 
 @Service
 public class PagamentoServices {
 	
+	@Value("${hr-worker.host}")
+	private String workerHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+		
 	public Pagamento getPagamento(long workerId, int dias) {
-		return new Pagamento("Bobo",200.0, dias);
+		Map<String, String>uriVariables = new HashMap<>();
+		uriVariables.put("id", ""+workerId);
+		
+		
+		Worker worker =restTemplate.getForObject(workerHost+"/workers/{id}",Worker.class,uriVariables);
+		return new Pagamento(worker.getNome(),worker.getDiaria(),dias);
 	}
 
 }
