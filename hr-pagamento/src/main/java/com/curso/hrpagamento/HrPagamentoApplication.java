ccbb8262628c7446ff42b595d8db0c843d8a75c4
dulcidio <dulcidio.sobrinho@gmail.com>
2022-03-18 08:34:44 -0300

    Microservico pagamento

---
+++ hr-pagamento/src/main/java/com/curso/hrpagamento/HrPagamentoApplication.java
@@ -0,0 +1,13 @@
+package com.curso.hrpagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrPagamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrPagamentoApplication.class, args);
+	}
+
+}
---
+++ hr-pagamento/src/test/java/com/curso/hrpagamento/HrPagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.hrpagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
